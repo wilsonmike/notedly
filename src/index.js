650e62780bb0c31a8aa6d3f9c17a611782e9f1d4
mike <michaelwilsonsj@hotmail.com>
2021-06-08 18:33:23 -0400

    dynamic port

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 // index.js
 // This is the main entry point of our application
 const express = require('express');
+const port = process.env.PORT || 4000;
 const app = express();
 
-app.get('/', (req, res) => res.send('react native'));
+app.get('/', (req, res) => res.send('react native 1'));
 
-app.listen(4000, () => console.log('listening on port 4000'));
+app.listen(port, () => (
+    console.log(`Server running at http://localhost:${port}`)
+));
