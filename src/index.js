bbb5549349cf72245ad28822c51b60ae6564ccd6
mike <michaelwilsonsj@hotmail.com>
2021-06-08 18:23:26 -0400

    express served locally

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 // index.js
 // This is the main entry point of our application
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('react native'));
+
+app.listen(4000, () => console.log('listening on port 4000'));
