2c3f128a9249d24ce52da7b7510e26d470cbf86e
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-09 16:53:07 -0400

    required apollo server express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 // index.js
 // This is the main entry point of our application
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const port = process.env.PORT || 4000;
 const app = express();
 
 app.get('/', (req, res) => res.send('react native 1'));
 
-app.listen(port, () => (
-    console.log(`Server running at http://localhost:${port}`)
-));
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
