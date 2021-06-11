a5c4e33a52c68a3009c5f96427483a83b19f6fb9
wilsonmike <michaelwilsonsj@hotmail.com>
2021-06-11 13:02:53 -0400

    added user model

---
+++ src/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      index: { unique: true }
+    },
+    email: {
+      type: String,
+      required: true,
+      index: { unique: true }
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    avatar: {
+      type: String
+    }
+  },
+  {
+    timestamps: true
+  }
+);
